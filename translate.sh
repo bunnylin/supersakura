e46321b3df351817a72d54e497ae756e6e0d876a
bunnylin <kirinn@mooncore.eu>
2017-06-12 22:23:14 +0100

    translate.sh: apply pre-translation substitutions, try again in case of translation timeouts

--- translate.sh
+++ translate.sh
@@ -10,18 +10,35 @@ printf "Phonetic\tGoogle\tBing\tYandex\n"
 
 while IFS= read -ru 10 line
 do
+  # Apply pre-translation substitutions.
+  while read -ru 11 sub
+  do
+    if ! echo $sub | grep -q "^ *#"
+    then
+      line=$(sed "s/$sub/g" <<< $line)
+    fi
+  done 11< <(grep . trans-subs.txt)
+
   # Replace backslashes with a double backslash, to avoid shell weirdness.
   line=$(sed "s/\\\/\\\\\\\/g" <<< $line)
 
-  transgoo=$(timeout 8 translate-shell ja:en -e google -no-ansi -no-autocorrect -show-alternatives n -show-languages n -show-prompt-message n -- "$line")
+  # Google translate may need a few tries occasionally.
+  tries=3
+  while [ $tries -gt 0 ]
+  do
+    tries=$(( tries-1 ))
+    transgoo=$(timeout 16 translate-shell ja:en -e google -no-ansi -no-autocorrect -show-alternatives n -show-languages n -show-prompt-message n -- "$line")
+    if [ "$transgoo" != "" ]; then tries=0; fi
+  done
+
   # $transgoo is now expected to have the original on line 1, the phonetic
   # on line 2 in brackets, and the translation on line 4, may be last line.
   trans0=$(printf -- "$transgoo" | sed -n 2p | sed "s/[()]//g")
   trans1=$(printf -- "$transgoo" | sed -n 4p)
 
   # Get the other translations.
-  trans2=$(timeout 8 translate-shell -b ja:en -e bing -no-ansi -- "$line")
-  trans3=$(timeout 8 translate-shell -b ja:en -e yandex -no-ansi -- "$line")
+  trans2=$(timeout 16 translate-shell -b ja:en -e bing -no-ansi -- "$line")
+  trans3=$(timeout 16 translate-shell -b ja:en -e yandex -no-ansi -- "$line")
 
   # Output a row.
   printf -- "$trans0\t$trans1\t$trans2\t$trans3\n"
